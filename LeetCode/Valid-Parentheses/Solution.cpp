71b7c3ed0072f4532ce16e23bd086eb10cabfbf8
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-08 03:37:54 +0900

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,1 +1,8 @@
-class Solu
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for (char c : s) {
+            if (mapping.find(c) == mapping.end()) {
