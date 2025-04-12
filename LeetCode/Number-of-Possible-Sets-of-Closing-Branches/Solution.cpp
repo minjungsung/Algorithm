98577ccb08756a4ec31c876dbaa844cbf375f446
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-13 00:43:36 +0900

    Add solution for Number of Possible Sets of Closing Branches

---
+++ LeetCode/Number-of-Possible-Sets-of-Closing-Branches/Solution.cpp
@@ -0,0 +1,9 @@
+Input: n = 3, maxDistance = 5, roads = [[0,1,2],[1,2,10],[0,2,10]]
+Output: 5
+Explanation: The possible sets of closing branches are:
+- The set [2], after closing, active branches are [0,1] and they are reachable to each other within distance 2.
+- The set [0,1], after closing, the active branch is [2].
+- The set [1,2], after closing, the active branch is [0].
+- The set [0,2], after closing, the active branch is [1].
+- The set [0,1,2], after closing, there are no active branches.
+It can be proven, that there are only 5 possible sets of closing branches.
