2e45bfb9695403c7ba65c4e8a2e7ce8b12e30653
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-13 00:43:09 +0900

    Add solution for Length of Longest Subarray With at Most K Frequency

---
+++ LeetCode/Length-of-Longest-Subarray-With-at-Most-K-Frequency/Solution.cpp
@@ -0,0 +1,4 @@
+Input: nums = [1,2,3,1,2,3,1,2], k = 2
+Output: 6
+Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+It can be shown that there are no good subarrays with length more than 6.
