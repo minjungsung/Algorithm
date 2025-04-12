9b7dd8233c5c1dc09561fedd57ab902c0fa50183
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-13 00:02:48 +0900

    Add solution for Gray Code

---
+++ LeetCode/Gray-Code/Solution.cpp
@@ -0,0 +1,13 @@
+Input: n = 2
+Output: [0,1,3,2]
+Explanation:
+The binary representation of [0,1,3,2] is [00,01,11,10].
+- 00 and 01 differ by one bit
+- 01 and 11 differ by one bit
+- 11 and 10 differ by one bit
+- 10 and 00 differ by one bit
+[0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
+- 00 and 10 differ by one bit
+- 10 and 11 differ by one bit
+- 11 and 01 differ by one bit
+- 01 and 00 differ by one bit
