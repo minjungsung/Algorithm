42441f83fdadb728fbc4561a75ec7b3150459446
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-15 00:10:04 +0900

    Add solution for Redistribute Characters to Make All Strings Equal

---
+++ LeetCode/Redistribute-Characters-to-Make-All-Strings-Equal/Solution.py
@@ -0,0 +1,5 @@
+Input: words = ["abc","aabc","bc"]
+Output: true
+Explanation: Move the first 'a' in words[1] to the front of words[2],
+to make words[1] = "abc" and words[2] = "abc".
+All the strings are now equal to "abc", so return true.
