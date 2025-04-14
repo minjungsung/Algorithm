01cfe2034f9755163838d81298ef2fea0e7ce14d
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-15 00:53:25 +0900

    Add solution for Check if Number Has Equal Digit Count and Digit Value

---
+++ LeetCode/Check-if-Number-Has-Equal-Digit-Count-and-Digit-Value/Solution.py
@@ -0,0 +1,8 @@
+Input: num = "1210"
+Output: true
+Explanation:
+num[0] = '1'. The digit 0 occurs once in num.
+num[1] = '2'. The digit 1 occurs twice in num.
+num[2] = '1'. The digit 2 occurs once in num.
+num[3] = '0'. The digit 3 occurs zero times in num.
+The condition holds true for every index in "1210", so return true.
