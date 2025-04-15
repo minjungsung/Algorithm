dc2ad74f70b8cf87948d8e396c5c6bc892b31e24
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-15 23:59:50 +0900

    Add solution for Maximum XOR With an Element From Array

---
+++ LeetCode/Maximum-XOR-With-an-Element-From-Array/Solution.cpp
@@ -0,0 +1,6 @@
+Input: nums = [0,1,2,3,4], queries = [[3,1],[1,3],[5,6]]
+Output: [3,3,7]
+Explanation:
+1) 0 and 1 are the only two integers not greater than 1. 0 XOR 3 = 3 and 1 XOR 3 = 2. The larger of the two is 3.
+2) 1 XOR 2 = 3.
+3) 5 XOR 2 = 7.
