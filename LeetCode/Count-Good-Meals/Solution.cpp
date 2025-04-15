08dc3c2c834a918b1732c01cddd41eaf7670671f
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-16 00:04:52 +0900

    Add solution for Count Good Meals

---
+++ LeetCode/Count-Good-Meals/Solution.cpp
@@ -0,0 +1,4 @@
+Input: deliciousness = [1,3,5,7,9]
+Output: 4
+Explanation: The good meals are (1,3), (1,7), (3,5) and, (7,9).
+Their respective sums are 4, 8, 8, and 16, all of which are powers of 2.
