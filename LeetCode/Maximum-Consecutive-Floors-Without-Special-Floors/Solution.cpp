a3c37d8abb942f588dcbd760ab779f9aa3d82c11
Minjung <147850869+minjungsung@users.noreply.github.com>
2025-04-16 23:56:37 +0900

    Add solution for Maximum Consecutive Floors Without Special Floors

--- LeetCode/Maximum-Consecutive-Floors-Without-Special-Floors/Solution.cpp
+++ LeetCode/Maximum-Consecutive-Floors-Without-Special-Floors/Solution.cpp
@@ -1,7 +1,22 @@
-Input: bottom = 2, top = 9, special = [4,6]
-Output: 3
-Explanation: The following are the ranges (inclusive) of consecutive floors without a special floor:
-- (2, 3) with a total amount of 2 floors.
-- (5, 5) with a total amount of 1 floor.
-- (7, 9) with a total amount of 3 floors.
-Therefore, we return the maximum number which is 3 floors.
+class Solution {
+public:
+int maxConsecutive(int b, int t, vector<int>& s) {
+    
+    sort(s.begin(), s.end());
+    
+    int ans=s[0]-b;
+    
+    
+    for(int i=1;i<s.size();i++){
+        
+        int x=s[i]-s[i-1]-1;
+        ans=max(ans, x);
+    }
+    
+    
+    ans=max(ans, t-s[s.size()-1]);
+    
+    
+    return ans;
+}
+};
